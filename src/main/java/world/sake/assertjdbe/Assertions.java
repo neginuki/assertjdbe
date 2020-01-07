235bc6f2b5d12c356fd0a985b5c8c8449a20b395
neginuki <mito.kunihiro@gmail.com>
2020-01-07 11:11:59 +0900

    no message

--- src/main/java/world/sake/assertjdbe/Assertions.java
+++ src/main/java/world/sake/assertjdbe/Assertions.java
@@ -3,14 +3,15 @@ package world.sake.assertjdbe;
 import java.util.function.Consumer;
 
 public class Assertions {
-    
-    private TestInfo testInfo;
-    
 
-    public static void assertThat(Runnable databaseChanges) {
-        databaseChanges.run();        
+    private TestInfo testInfo;
+
+    public static DatabaseChangesAssert assertThat(Runnable databaseChanges) {
+        databaseChanges.run();
+
+        return new DatabaseChangesAssert();
     }
-    
+
     public void settings(Consumer<TestInfo> testInfo) {
 
     }
--- src/test/java/world/sake/assertjdbe/AssertionsJunit3Test.java
+++ src/test/java/world/sake/assertjdbe/AssertionsJunit3Test.java
@@ -6,6 +6,8 @@ public class AssertionsJunit3Test extends AbstractAssertionsJunit3Test {
 
     @Test
     public void test_case1() {
-        System.out.println("OK");
+        Assertions.assertThat(() -> {
+            // database update
+        }).isEquals();
     }
 }
